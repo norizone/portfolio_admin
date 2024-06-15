795bb00f5a13fbb41e2c87f2de16570519ab559b
minami <minami.takanori.1113@gmail.com>
2024-06-15 16:06:31 +0900

    update: ユーザー管理ページ追加

--- src/app/user_management/page.tsx
+++ src/app/user_management/page.tsx
@@ -1,1 +1,31 @@
-// user管理画面
+'use client'
+import { BaseBtn } from '@/components/btn/BaseBtn'
+import { PrimaryHeadline } from '@/components/headline/PrimaryHeadline'
+import { BasePagination } from '@/components/pagination/BasePagination'
+import { BaseTable } from '@/components/table/BaseTable'
+
+export default function UserManagement() {
+  return (
+    <section>
+      <PrimaryHeadline lang="jp" tag="h1">
+        ユーザー一覧
+      </PrimaryHeadline>
+      <div className="mt-[2em] text-right">
+        <BaseBtn
+          onClick={(e) => {
+            e.preventDefault
+          }}
+          btnColor="primary"
+        >
+          新規作成
+        </BaseBtn>
+      </div>
+      <div className="mt-[2em]">
+        <BaseTable />
+      </div>
+      <div className="mt-[2em]">
+        <BasePagination allPage={20} currentPage={3} />
+      </div>
+    </section>
+  )
+}
