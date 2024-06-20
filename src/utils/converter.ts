f0d33052c1061eb174f6d0ea765260d911ca4f69
minami <minami.takanori.1113@gmail.com>
2024-06-21 06:03:25 +0900

    enum 追加

--- src/utils/converter.ts
+++ src/utils/converter.ts
@@ -1,6 +1,19 @@
-import { PUBLICATION_STATUS } from './enum'
+import { PUBLICATION_STATUS, USER_ROLE, VIEW_PERMISSION } from './enum'
 
 export const convertPublication: Record<PUBLICATION_STATUS, string> = {
   [PUBLICATION_STATUS.PUBLIC]: '公開',
   [PUBLICATION_STATUS.PRIVATE]: '非公開',
-};
+}
+
+export const convertViewPermission: Record<VIEW_PERMISSION, string> = {
+  [VIEW_PERMISSION.GUEST]: '未ログイン',
+  [VIEW_PERMISSION.LIMIT]: '制限あり',
+  [VIEW_PERMISSION.ALL]: '管理者権限以外すべて',
+  [VIEW_PERMISSION.ADMIN]: '管理者権限',
+}
+
+export const convertUserRole: Record<USER_ROLE, string> = {
+  [USER_ROLE.LIMIT_VIEWING]: '制限付きユーザー',
+  [USER_ROLE.ALL_VIEWING]: '制限なしユーザー',
+  [USER_ROLE.ADMIN]: '管理者',
+}
