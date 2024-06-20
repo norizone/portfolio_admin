b9f2a1e405be250c776d5a9d81c177c870fcd375
minami <minami.takanori.1113@gmail.com>
2024-06-21 06:01:46 +0900

    style追加

---
+++ src/styles/style.ts
@@ -0,0 +1,5 @@
+export const styleInputMargin = 'mx-[1em] mt-[.2em] w-[calc(100%_-_2em)]'
+export const styleMinInputWidth = 'w-[16em]'
+export const styleFormBgWhite = 'bg-white shadow-md'
+export const styleModalFormWidth = 'w-[600px]'
+export const stylePageFormWidth = 'w-[90%]'
