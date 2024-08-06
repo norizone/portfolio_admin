47dec6327195472c791ee37327f34b6f29e58288
minami <minami.takanori.1113@gmail.com>
2024-08-06 22:35:58 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,44 +1,39 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
-import { routers } from './routers/routers'
-import axios from 'axios'
-import { authApiUrl, baseURL } from './utils/apiUrl'
+// import { NextResponse } from 'next/server'
+// import type { NextRequest } from 'next/server'
+// import { routers } from './routers/routers'
+// import axios from 'axios'
+// import { authApiUrl, baseURL } from './utils/apiUrl'
 
-export const config = {
-  matcher: ['/((?!login|signup|_next/static|_next/image|favicon.ico).*)'],
-}
+// export const config = {
+//   matcher: ['/((?!login|signup|_next/static|_next/image|favicon.ico).*)'],
+// }
 
-export function middleware(request: NextRequest) {
-  const response = NextResponse.next()
+// export function middleware(request: NextRequest) {
+//   const response = NextResponse.next()
 
-  const cookieToken = request.cookies.get('access_token')
-  console.log('token', cookieToken?.value)
-  if (!cookieToken?.value)
-    return NextResponse.redirect(`${request.nextUrl.origin}${routers.LOGIN}`)
+//   const cookieToken = request.cookies.get('access_token')
+//   if (!cookieToken?.value)
+//     return NextResponse.redirect(`${request.nextUrl.origin}${routers.LOGIN}`)
 
-  const cookie = request.cookies
-    .getAll()
-    .map((cookie) => `${cookie.name}=${cookie.value}`)
-    .join('; ')
-  return response
-  const getAuth = async () => {
-    try {
-      const res = await axios.get(`${baseURL}${authApiUrl.default}`, {
-        headers: { cookie },
-        withCredentials: true,
-      })
-      if (res.status === 200) {
-        console.log('success')
-        return response
-      } else {
-        console.log('error')
-        return NextResponse.redirect(
-          `${request.nextUrl.origin}${routers.LOGIN}`
-        )
-      }
-    } catch (error) {
-      return NextResponse.redirect(`${request.nextUrl.origin}${routers.LOGIN}`)
-    }
-  }
-  getAuth()
-}
+//   const getAuth = async () => {
+//     const cookie = request.cookies
+//       .getAll()
+//       .map((cookie) => `${cookie.name}=${cookie.value}`)
+//       .join('; ')
+//     try {
+//       const res = await axios.get(`${baseURL}${authApiUrl.default}`, {
+//         headers: { cookie },
+//       })
+//       if (res.status === 200) {
+//         return response
+//       } else {
+//         return NextResponse.redirect(
+//           `${request.nextUrl.origin}${routers.LOGIN}`
+//         )
+//       }
+//     } catch (error) {
+//       return NextResponse.redirect(`${request.nextUrl.origin}${routers.LOGIN}`)
+//     }
+//   }
+//   getAuth()
+// }
