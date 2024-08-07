56067f9d79334d1bbf117784361750261ba19144
minami <minami.takanori.1113@gmail.com>
2024-08-07 09:54:16 +0900

    test

--- src/providers/authGuard.tsx
+++ src/providers/authGuard.tsx
@@ -9,22 +9,23 @@ import { useEffect } from 'react'
 export const AuthGuard = (props: { children: React.ReactNode }) => {
   const pathname = usePathname()
   const router = useRouter()
-  const { isLoading, isError } = useGetAuth(
-    pathname !== routers.LOGIN && pathname !== routers.SIGNUP,
-    true,
-  )
+  const isLoading = false
+  // const { isLoading, isError } = useGetAuth(
+  //   pathname !== routers.LOGIN && pathname !== routers.SIGNUP,
+  //   true,
+  // )
 
-  const {
-    onLogout
-  } = useLogout({ onSuccessLogout: () => router.replace(routers.LOGIN) })
+  // const {
+  //   onLogout
+  // } = useLogout({ onSuccessLogout: () => router.replace(routers.LOGIN) })
 
-  useEffect(() => {
-    if (isLoading || !isError) return
-    // onLogout()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isError, isLoading])
+  // useEffect(() => {
+  //   if (isLoading || !isError) return
+  //   // onLogout()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [isError, isLoading])
 
-  if (isError) return (<></>)
+  // if (isError) return (<></>)
 
   return (
     isLoading ? <LoadingIcon /> : <>{props.children}</>
